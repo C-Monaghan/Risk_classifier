5ee947e4ccf24f9f254eac1e7969f63fb3f3382f
Sajal <sajal.minhas@ucdconnect.ie>
2020-10-01 12:09:18 +0100

    Function Ready

--- Main.R
+++ Main.R
@@ -11,7 +11,7 @@ library("reticulate")
 library("tidyrules")
 library("dplyr")
 library("pander")
-
+library(caret)
 
 load("GermanCredit.Rdata")
 
@@ -74,141 +74,51 @@ Classifier<-function(default_data,choose_regression = TRUE) {
   }
   ###################### Decision tree
   
-  return(list(Data=dim(default_data)))
+  # You get the names of the columns
+  Cols <- names(default_data)
+  Cols <- Cols[! Cols %in% "Class"]
+  n <- length(Cols)
+  
+  
+  # You construct all possible combinations
+  id <- unlist( lapply(1:n,function(i)combn(1:n,i,simplify=FALSE)) ,
+                recursive=FALSE)
+  id<-sample(id, 1000, replace=FALSE)
+  
+  # You paste them to formulas
+  Formulas <- sapply(id,function(i)
+    paste("Class~",paste(Cols[i],collapse="+")))
+  
+  # Storing all the combination of trees
+  Forest = list()
+  for(i in 1:1000) {
+    RPI = rpart(Formulas[[i]],data=default_data,method = "class")
+    Forest[[i]] = RPI
+  }
+  
+  #~~~~~~~~  TESTING PERFORMANCE
+  pred = list()
+  for(i in 1:1000) {
+    RPI<-predict(Forest[i],type="class")
+    pred[[i]] = RPI
+  }
+  
+  tab= list()
+  for(i in 1:1000) {
+    RP<-table(default_data[,1],pred[[i]][[1]])
+    tab[[i]] = RP
+  }
+  
+  res= list()
+  for(i in 1:1000) {
+    RP<-sum(diag(tab[[i]]))/sum(tab[[i]])
+    res[[i]] = RP
+  }
+  
+  res<-unlist(res, use.names=FALSE)
+  
+  return(list(Data=dim(default_data), Trees=Forest,Performance=res))
 }
-
-# Trying decision trees
-prop.table(table(default_data[,1]))
-
-# Trying decision trees
-## Recursive Partitioning and Regression Trees
-# Fit the model
-fit.r <- rpart(default_data[,1]~.,data=default_data)
-
-# Examine the output
-fit.r
-summary(fit.r)
-
-# Plot the tree
-plot(fit.r)
-text(fit.r,use.n=TRUE,cex=0.5,xpd=TRUE,col="red")
-
-# Fit the model
-fit.c <- ctree(default_data[,1]~.,data=default_data)
-
-# Examine the output
-fit.c
-summary(fit.c)
-
-# Plot the tree
-plot(fit.c)
-
-# ~~~~~ Recursive Partitioning and Regression Trees
-# Fit the model
-fit.r <- rpart(default_data[,1]~.,data=default_data)
-
-# ~~~~~ Conditional Inference Trees
-# Fit the model
-fit.c <- ctree(default_data[,1]~.,data=default_data)
-
-# You get the names of the columns
-Cols <- names(default_data)
-Cols <- Cols[! Cols %in% "Class"]
-n <- length(Cols)
- 
-
-# You construct all possible combinations
-id <- unlist( lapply(1:n,function(i)combn(1:n,i,simplify=FALSE)) ,
-              recursive=FALSE)
-id<-sample(id, 1000, replace=FALSE)
-
-# You paste them to formulas
-Formulas <- sapply(id,function(i)
-  paste("Class~",paste(Cols[i],collapse="+")))
-
-
-aa<-lapply(Formulas,function(i) ctree(as.formula(i),data=default_data))
-bb<- lapply(Formulas,function(i) rpart(as.formula(i),data=default_data))
-getwd()
-
-
-# save(aa, file="ctree.RData")
-# save(bb, file="rpart.RData")
-# 
-# load("ctree.RData")
-# load("rpart.RData")
-# 
-# write.csv(ctree,file="ctree.csv")
-# write.csv(rpart,file="rpart.csv")
-
-
-Forest = list()
-for(i in 1:1000) {
-  RPI = rpart(Formulas[[i]],data=default_data,method = "class")
-  Forest[[i]] = RPI
-}
-
-
-save(Forest,file = "Forest.RData" )
-load("Forest.RData")
-write.csv(Forestm,file="Forest.csv")
-write.table(Forest,file = "Forest.csv")
-library(data.table)
-fwrite(Forest,file = "Forest.csv",sep = " " , sep2 = c("","|",""), row.names = T,quote = F,col.names = TRUE)
-
-#~~~~~~~~  TESTING PERFORMANCE
-library(caret)
-pred = list()
-for(i in 1:1000) {
-  RPI<-predict(Forest[i],type="class")
-  pred[[i]] = RPI
-}
-
-tab= list()
-for(i in 1:1000) {
-  RP<-table(default_data[,1],pred[[i]][[1]])
-  tab[[i]] = RP
-}
-
-res= list()
-for(i in 1:1000) {
-  RP<-sum(diag(tab[[i]]))/sum(tab[[i]])
-  res[[i]] = RP
-}
-
-res<-unlist(res, use.names=FALSE)
-summary(res)
-write.table(res,sep="",file = "res.csv",row.names = FALSE)
-
-rpart.plot(bb[[1]])
-plotcp(bb[[1]])
-A<-as.list(aa)
-B<-as.list(bb)
-
-
-
-prp(fit.r, extra = 1)
-plotcp(fit.r)
-
-predict_red_org<-predict(fit.r,type="class")
-tab_red_org<-table(default_data[,1],predict_red_org)
-res_red_org<-sum(diag(tab_red_org))/sum(tab_red_org)
-res_red_org
-confusionMatrix(data = predict_red_org, reference = default_data[,1])
-
-
-pred_a<-predict(A[[100]])
-tab_b<-table(default_data[,1],pred_b)
-res_a<-sum(diag(tab_a))/sum(tab_a)
-res_a
-confusionMatrix(data = pred_a, reference = default_data[,1])
-
-
-pred_b<-predict(B[[100]],type="class")
-tab_a<-table(default_data[,1],pred_a)
-res_b<-sum(diag(tab_b))/sum(tab_b)
-res_b
-confusionMatrix(data = pred_b, reference = default_data[,1])
 
 
 #interpretation
